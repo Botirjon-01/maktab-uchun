c2c7285f67cc30385f339a61c7de841f74229fdc
Botirjon <botirjontursunaliyev1998@gamail.com>
2023-06-12 20:54:05 +0500

    maktab sayt

---
+++ menular/quiz test/js/question.js
@@ -0,0 +1,157 @@
+
+
+// Array of Objects
+const quiz = [
+  {
+    q: 'Orta Osiyo geografiyasiga kim asos solgan ?',
+    options:['Abu Rayxon Beruniy ','Ibn Sino','Erotosfen','Xorazmiy'],
+    answer:0
+  },
+  {
+     q: 'O`rta Osiyodagi eng kop sonli va eng yirik millat qaysi?',
+     options:['qozoqlar', 'tojiklar', 'ozbeklar', 'qirg`izlar'],
+     answer:2
+  },
+  {
+     q: 'Hozirgi dunyo xaritasida nechta davlat bor?',
+     options:[' 170 ta','140 ta','230 ta','150 ta'],
+     answer:2
+   },
+   {
+     q: 'Tayanch mamlakatlarini aniqlang?',
+     options:['Angliya, Fransiya','Hindiston, Braziliya','AQSh, Kanada','Italiya, Ispaniya'],
+     answer:1
+   },
+   {
+     q: 'Tugamaydigan resurslarga nimalar kiradi?',
+     options:['Osimlik va hayvonot dunyosi','Shamol, quyosh, yerning ichki energiyasi','Mineral tuzlar, suv','Barchasi togri'],
+     answer:1
+   },
+   {
+     q: 'Jahon yer fondining umumiy maydoni qanchaga teng?',
+     options:['15 milyard gektar','11 milyard gektar','9,5 milyard gektar','13,4 milyard gektar'],
+     answer:3
+   },
+   {
+     q: '"Tinchlik yolida hamkorlik"dasturi qaysi tashkilot tomonidan qabul qilingan?',
+     options:['BMT','Yunesko','Nato','MDH'],
+     answer:2
+   },
+   {
+     q: 'Jahondagi aholisi eng yirik shahar qaysi?',
+     options:['Tokio ','Mexiko','London','Nyu-York'],
+     answer:0
+   },
+   {
+     q: 'Aholisi zichligi boyicha eng yuqori bo`lgan hudud?',
+     options:['Yevropa','Avstraliya','Osiyo','Shimoliy Amerika'],
+     answer:0
+   },
+   {
+     q: 'Avstraliya qanday manoni anglatadi',
+     options:['Janubiy-Sharqiy','Janubiy','Shimoliy','Materik orol'],
+     answer:3
+   },
+   {
+     q: 'Quyidagi toglar ichida eng balandini aniqlang',
+     options:['Kavkaz','Pomir','Karpat','Himolay'],
+     answer:3
+   },
+   {
+     q: 'Shimoliy Amerikada eng sersuv daryo qaysi?',
+     options:['Kalarado','Kolumbiya','Yukon','Missisipi'],
+     answer:0
+   },
+   {
+     q: 'Yer shariningquruqlikdagi eng past nuqtasi qayerda joylashgan?',
+     options:['Olik dengiz','Kaspiy dengizi','Mariana botigi','Assal dengizi'],
+     answer:2
+   },
+   {
+     q: 'Avstraliya materigining necha foizi berk havza?',
+     options:['40%','50%','60%','70%'],
+     answer:2
+   },
+   {
+     q: 'Avstraliya materigi berk havzasi boyicha dunyoda nechanchi orinda turadi?',
+     options:['4','3','2','1'],
+     answer:3
+   },
+   {
+     q: 'Quyosh Yerdan necha marta katta?',
+     options:['1 mln 500 ming marta','1 mln 400 ming marta','1 mln 300 ming marta','1 mln 200 ming marta'],
+     answer:0
+   },
+   {
+     q: 'Yerdan Quyoshgacha bo’lgan masofa qancha?',
+     options:['150 mln km','160 mln km','165 mln km','170 mln km'],
+     answer:0
+   },
+   {
+     q: 'Yer yuzadagi eng baland tog’ cho’qqisini aniqlang?',
+     options:['Klimanjaro cho’qqisi','Mak-Kinli cho’qqisi','Jomolungma cho’qqisi','Akonkagua cho’qqisi'],
+     answer:2
+   },
+   {
+     q: '"Geografiya” atamasini birinchi bo’lib fanga kim kiritgan?',
+     options:['Falas','Pifagor','Arastu','Eratosfen'],
+     answer:3
+   },
+   {
+     q: 'Hozirgi vaqtda dunyo siyosiy haritasida nechta mustaqil davlat bor?',
+     options:['150 ta','169 ta','189 ta','220 ta'],
+     answer:1
+   },
+   {
+     q: 'Yangi Zelandiyani poytaxti qaysi javobda to’g’ri ko’rsatilgan?',
+     options:['Bellington','Rim','Tokio','Islomobod'],
+     answer:0
+   },
+   {
+     q: 'Unitar so’zining ma’nosi nima?',
+     options:['ittifoq','birlashma','yaxlit, yagona','to’g’ri javob yo’q'],
+     answer:2
+   },
+   {
+     q: 'Dunyoda aholi soni bo’yicha eng ko’p bo’lgan davlat qaysi?',
+     options:['Xitoy','Hindiston','AQSH','Yaponiya'],
+     answer:0
+   },
+   {
+     q: 'O’zbekistonda aholining umr ko’rish yoshi qancha?',
+     options:['69','70','75','80'],
+     answer:0
+   },
+   {
+     q: 'O’zbekistonga qo’shni davlatlarni belgilang.',
+     options:['Tojikiston','Turkmaniston Qozog’iston','Avgoniston','hamma javob to’g’ri'],
+     answer:3
+   },
+   {
+     q: 'O’rmonlar yerning qaysi qismida ko’p joylashgan?',
+     options:['sharqiy','g’arbiy','shimoliy','janubiy'],
+     answer:1
+   },
+   {
+     q: 'Olmos qayerdan qazib olinadi?',
+     options:['Litva','Latviya','Namibiya','Indoneziya'],
+     answer:3
+   },
+   {
+     q: 'Rossiyaning chegaradosh davlatlari nechta?',
+     options:['4','8','12','16'],
+     answer:3
+   },
+   {
+     q:'Osiyo so’zining ma’nosi?',
+     options:['sharq, kun chiqar','g’arb, kun botar','shimol','janub'],
+     answer:0
+   },
+   {
+     q:'Xitoyda qancha millat va elat yashaydi?',
+     options:['60 dan ortiq','100 dan ortiq','120 dan ortiq','150 dan ortiq'],
+     answer:0
+   }
+
+  
+]
